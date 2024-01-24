6591758304cbc4e9ea476829b5dbea90d7904945
CraigB73 <craigbritton73@gmail.com>
2024-01-24 12:55:45 +0100

    Add header to homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { HomePage } from './pages/homepage';
 
 function App() {
   return (
     <div className="App">
- 
+      <HomePage />
     </div>
   );
 }
---
+++ src/pages/homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './page-style.css'
+
+export const HomePage = () => {
+  return (
+      <div className='bg-homepage'>HomePage
+         <div className='heading-wrapper'>
+           <h1>Craig</h1>
+           <h2 className='lastname'>Britton</h2>
+           <p>FRONT-END DEVELOPER</p>  
+         </div>
+      </div>
+  )
+}
